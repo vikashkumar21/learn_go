808f4a3ebff5d38905904064e7140b0f4ca1ba68
thisisvikashkr <vikash.kumar@jobrapido.com>
2024-09-23 17:47:10 +0530

    pointers, goroutine

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -51,6 +54,13 @@ func main() {
 	arrayConcept()
 	sliceConcept()
 	mapConcept()
+	stringConcept()
+	var rect = rectangle{4, 5}
+	var cir = circle{5}
+	fmt.Println(calculateArea(rect))
+	fmt.Println(calculateArea(cir))
+	pointerConcept()
+	goRoutineConcept()
 }
 
 func intDivision(numerator, denominator int) (int, int, error) {
@@ -103,4 +113,105 @@ func mapConcept() {
 	for i := 0; i < 10; i++ {
 		fmt.Print(i)
 	}
+}
+
+func stringConcept() {
+	var str = "resume"
+	fmt.Println()
+	fmt.Printf("%v %T\n", str[0], str[0])
+	for i, v := range str {
+		fmt.Println(i, v)
+	}
+	fmt.Printf("The length of the string is %v", len("resume"))
+	var runeStr = 'e'
+	fmt.Println(runeStr)
+	var stringBuilder strings.Builder
+	var stringSlice = []string{"a", "b", "c"}
+	for i := range stringSlice {
+		stringBuilder.WriteString(stringSlice[i])
+	}
+	var catString = stringBuilder.String()
+	fmt.Println(catString)
+}
+
+type rectangle struct {
+	length int
+	width  int
+}
+
+type circle struct {
+	radius int
+}
+
+func (r rectangle) area() int {
+	return r.length * r.width
+}
+
+func (c circle) area() int {
+	return 3 * c.radius
+}
+
+type shape interface {
+	area() int
+}
+
+func calculateArea(s shape) int {
+	return s.area()
+}
+
+func pointerConcept() {
+	var p *int32 = new(int32)
+	var i int32
+	fmt.Printf("The value p points to %v\n", *p)
+	fmt.Printf("The value of i is %v\n", i)
+	p = &i
+	*p = 1
+	fmt.Printf("The value p points to %v\n", *p)
+	fmt.Printf("The value of i is %v\n", i)
+	var slice = []int32{1, 2, 3}
+	var sliceCopy = slice
+	sliceCopy[2] = 4
+	fmt.Println(slice)
+	fmt.Println(sliceCopy)
+	arr := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("The main arr address is %p\n", &arr)
+	var result = square(&arr)
+	fmt.Printf("arr in main:  %v\n", arr)
+	fmt.Printf("The result arr address is %p\n", &result)
+	fmt.Printf("Result in main: %v\n", result)
+
+}
+
+func square(arr *[5]int) [5]int {
+	fmt.Printf("The square arr address is %p\n", arr)
+	for i := range arr {
+		arr[i] = arr[i] * arr[i]
+	}
+	return *arr
+}
+
+var mutex = sync.Mutex{}
+var wg = sync.WaitGroup{}
+var dbData = []string{"abc", "def", "ghi", "jkl", "mno"}
+var result = []string{}
+
+func goRoutineConcept() {
+	t0 := time.Now()
+	for i := 0; i < len(dbData); i++ {
+		wg.Add(1)
+		go dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("Total execute time: %v\n", time.Since(t0))
+	fmt.Printf("Results: %v\n", result)
+}
+
+func dbCall(i int) {
+	var delay float32 = 2000
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Printf("The result from the database is: %v\n", dbData[i])
+	mutex.Lock()
+	result = append(result, dbData[i])
+	mutex.Unlock()
+	wg.Done()
 }
